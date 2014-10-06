d5240787c2f7cca382f7384165948483b6f3d704
Ian Turgeon <iturgeon@gmail.com>
2014-10-06 13:37:36 -0400

    adds from address to emailer

--- test_mail_basic.php
+++ test_mail_basic.php
@@ -11,8 +11,9 @@ if (isset($_POST['score'], $_POST['first_name'], $_POST['last_name']))
 	$subject    = "ENC 6217-Module 3: Copyediting Grade";
 	$name       = trim($_POST['first_name']) . ' ' . trim($_POST['last_name']);
 	$body       = str_replace('/', '', "$name received: {$_POST['score']} out of 10.");
+	$headers    = "From: Copy Editing Flash Widget <noReply@ucf.edu>\r\n";
 
-	if ( mail($to, $subject, $body) )
+	if ( mail($to, $subject, $body, $headers) )
 	{
 		echo "Thank you. Your score has been submitted to the instructor. You may close this window.";
 	}
