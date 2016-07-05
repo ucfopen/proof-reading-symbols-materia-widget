927d508cbd16c7d3e8f9afde96b36712a1f6b298
Shauna Mason <shauna.mason@ucf.edu>
2016-07-05 12:27:11 -0400

    Updated file

--- _score/score_module.php
+++ _score/score_module.php
@@ -43,36 +43,4 @@ class Score_Modules_ProofReadingSymbols extends Score_Module
 
 		return 0;
 	}
-
-	protected function handle_log_question_answered($log)
-	{
-		if ($this->q_ids == null) $this->q_ids = array_keys($this->questions);
-
-		$this->total_questions++;
-		$this->verified_score += $this->check_answer($log); // score the question and add it to the total
-
-		// find current log's index from original quetion's array and add to new array
-		$q_index = array_search($log->item_id, $this->q_ids);
-		$this->new_logs[$q_index] = $log;
-	}
-
-	protected function calculate_score()
-	{
-		ksort($this->new_logs);
-		$this->logs = $this->new_logs;
-
-		$global_mod = array_sum($this->global_modifiers);
-		// if ( ! is_numeric($mod)) $mod = 0;
-		if ($this->total_questions > 0)
-		{
-			$points = $this->verified_score + $global_mod * $this->total_questions;
-			$this->calculated_percent = $points / $this->total_questions;
-		}
-		else
-		{
-			$points = 100 + $this->verified_score + $global_mod;
-			$this->calculated_percent = $points;
-		}
-		if ($this->calculated_percent < 0) $this->calculated_percent = 0;
-	}
 }
--- _score/test_score_module.php
+++ _score/test_score_module.php
@@ -5,55 +5,41 @@
  * @group Score
  * @group HelloWidget
  */
-class Test_Score_Modules_ProofReadingSymbols extends \Basetest
+class Test_Score_Modules_Sequencer extends \Basetest
 {
-
-	protected function _get_qset()
-	{
-
-		return json_decode('');
-	}
 
 	protected function _makeWidget($partial = 'false')
 	{
 		$this->_asAuthor();
 
-		$title = 'PROOF READING SYMBOLS MODULE';
-		$widget_id = $this->_find_widget_id('ProofReadingSymbols');
+		$title = 'SEQUENCER SCORE MODULE TEST';
+		$widget_id = $this->_find_widget_id('Sequencer');
 		$qset = (object) ['version' => 1, 'data' => $this->_get_qset()];
 		return \Materia\Api::widget_instance_save($widget_id, $title, $qset, false);
 	}
 
 	public function test_check_answer()
 	{
+		$this->_test_full_credit();
+		$this->_test_partial_credit();
+	}
+
+	function _test_full_credit() {
 		$inst = $this->_makeWidget('false');
 		$play_session = \Materia\Api::session_play_create($inst->id);
 		$qset = \Materia\Api::question_set_get($inst->id, $play_session);
 
 		$logs = array();
 
-		$logs[] = json_decode('{
-			"text":"50",
-			"type":1004,
-			"value":"",
-			"item_id":"'.$qset->data['items'][0]['items'][0]['id'].'",
-			"game_time":10
-		}');
-		$logs[] = json_decode('{
-			"text":"ONE HUNDRED5",
-			"type":1004,
-			"value":"",
-			"item_id":"'.$qset->data['items'][0]['items'][1]['id'].'",
-			"game_time":10
-		}');
-
-		$logs[] = json_decode('{
-			"text":"",
-			"type":2,
-			"value":"",
-			"item_id":0,
-			"game_time":12
-		}');
+		for ($i=1;$i<10;$i++) {
+			$logs[] = json_decode('{
+				"text":"'.$i.'",
+				"type":1004,
+				"value":"",
+				"item_id":"'.$qset->data['qset']['data']['items'][$i - 1]['id'].'",
+				"game_time":10
+			}');
+		}
 
 		$output = \Materia\Api::play_logs_save($play_session, $logs);
 
@@ -62,6 +48,43 @@ class Test_Score_Modules_ProofReadingSymbols extends \Basetest
 		$this_score = \Materia\Api::widget_instance_play_scores_get($play_session);
 
 		$this->assertInternalType('array', $this_score);
-		$this->assertEquals(25, $this_score[0]['overview']['score']);
+		$this->assertEquals(100, $this_score[0]['overview']['score']);
+	}
+
+	function _test_partial_credit() {
+		$inst = $this->_makeWidget('false');
+		$play_session = \Materia\Api::session_play_create($inst->id);
+		$qset = \Materia\Api::question_set_get($inst->id, $play_session);
+
+		$logs = array();
+
+		# Test to make sure that multiple attempts will dock the score
+		for ($i=1;$i<10;$i++) {
+			$logs[] = json_decode('{
+				"text":"'.$i.'",
+				"type":1004,
+				"value":"",
+				"item_id":"'.$qset->data['qset']['data']['items'][$i - 1]['id'].'",
+				"game_time":10
+			}');
+		}
+		for ($i=0;$i<3;$i++) {
+			$logs[] = json_decode('{
+				"text":"attempt_penalty",
+				"type":1001,
+				"value":-'.$qset->data['qset']['data']['options']['penalty'].',
+				"item_id":null,
+				"game_time":'.(10 + $i).'
+			}');
+		}
+
+		$output = \Materia\Api::play_logs_save($play_session, $logs);
+
+		$scores = \Materia\Api::widget_instance_scores_get($inst->id);
+
+		$this_score = \Materia\Api::widget_instance_play_scores_get($play_session);
+
+		$this->assertInternalType('array', $this_score);
+		$this->assertEquals(70, $this_score[0]['overview']['score']);
 	}
 }
