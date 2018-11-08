ad8a57e3ecd4c16b2d2f1d8228b59de1cd78e886
Ian Turgeon <iturgeon@gmail.com>
2018-11-07 20:14:27 -0500

    refactor qset to be backed into widget, update score module, some angular polish

--- src/_score/score_module.php
+++ src/_score/score_module.php
@@ -1,34 +1,9 @@
 <?php
-/**
- * Materia
- * It's a thing
- *
- * @package	    Materia
- * @version    1.0
- * @author     UCF New Media
- * @copyright  2011 New Media
- * @link       http://kogneato.com
- */
-
-
-/**
- * NEEDS DOCUMENTATION
- *
- * The widget managers for the Materia package.
- *
- * @package	    Main
- * @subpackage  scoring
- * @category    Modules
-  * @author     ADD NAME HERE
- */
 
 namespace Materia;
 
 class Score_Modules_ProofReadingSymbols extends Score_Module
 {
-
-	private $new_logs = [];
-	private $q_ids = null;
 
 	public function check_answer($log)
 	{
@@ -42,5 +17,63 @@ class Score_Modules_ProofReadingSymbols extends Score_Module
 		}
 
 		return 0;
+	}
+
+	protected function details_for_question_answered($log)
+	{
+		// old version of qset uses default details_for_question_answered function
+		if (isset($this->questions[$log->item_id])){
+			return parent::details_for_question_answered($log);
+		}
+
+		// new version uses responses in the log
+		$score = $log->value === $log->text ? 100 : 0;
+
+		return [
+			'data' => [
+				$log->item_id,
+				$log->text
+			],
+			'data_style'    => ['question', 'response', 'answer'],
+			'score'         => $score,
+			'type'          => $log->type,
+			'style'         => $this->get_detail_style($score),
+			'tag'           => 'div',
+			'symbol'        => '%',
+			'graphic'       => 'score',
+			'display_score' => true
+		];
+	}
+
+	protected function get_score_details()
+	{
+		$details = [];
+
+		foreach ($this->logs as $log)
+		{
+			switch ($log->type)
+			{
+				case Session_Log::TYPE_QUESTION_ANSWERED:
+					$details[] = $this->details_for_question_answered($log);
+					break;
+			}
+		}
+
+		if (count($details[0]) && count($details[0]['data']) === 2)
+		{
+			// new scores only have 2 data points
+			$headers = ['Question Score', 'The Question', 'You Selected'];
+		}
+		else
+		{
+			// old scores have 3 data points
+			$headers = $this->_ss_table_headers;
+		}
+
+		return [[
+				'title'  => $this->_ss_table_title,
+				'header' => $headers,
+				'table'  => $details
+		]];
 	}
 }
